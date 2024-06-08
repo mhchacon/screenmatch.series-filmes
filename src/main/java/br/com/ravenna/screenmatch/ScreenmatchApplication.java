c9827c72c3c2496d870bba789a97d63c2fb10569
mhchacon <mariana.chacon@ufrpe.br>
2024-06-08 16:10:54 -0300

    add dados series/filmes/temporadas

--- src/main/java/br/com/ravenna/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/ravenna/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,8 @@
 package br.com.ravenna.screenmatch;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,6 +23,19 @@ public class ScreenmatchApplication implements CommandLineRunner{
 		ConverteDados conversor = new ConverteDados();
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6608c387");
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for(int i = 1 ; i<= dados.totalTemporadas(); i++){
+			json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=6608c387");
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+
+		}
+		temporadas.forEach(System.out::println);
 	}
 
 }
